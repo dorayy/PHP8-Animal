adab8a73de2501783f82ecdddaf05391133aca7f
doray <d.balassoupramanien@ecole-ipssi.net>
2021-10-14 14:49:19 +0200

    match

---
+++ src/bonus.php
@@ -0,0 +1,15 @@
+<?php
+
+// Utilisation de match
+
+$age = 17;
+
+$result = match (true) {
+    $age >= 65 => 'senior',
+    $age >= 25 => 'adult',
+    $age >= 18 => 'young adult',
+    default => 'kid',
+};
+
+var_dump($result);
+
